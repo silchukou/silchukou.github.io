8c3811b5374d98c4c58e40e2bb4d2cd447e05a3d
Eugene Malinouski <jenyamalina@tut.by>
2023-08-29 23:13:00 +0200

    Add prints

--- scripts/instagram/update.py
+++ scripts/instagram/update.py
@@ -4,11 +4,17 @@ import json
 
 
 def update_insta():
+    print("Updating Instagram...")
     url = """https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"242754263","first":8,"after":null}"""
     response = requests.get(url)
 
-    clear_images()
+    print(f"Fetched Instagram data with status code {response.status_code}")
 
+    print("Clearing images...")
+    clear_images()
+    print("Cleared images")
+
+    print("Formatting data...")
     posts = []
     for edge in response.json()["data"]["user"]["edge_owner_to_timeline_media"][
         "edges"
@@ -16,19 +22,23 @@ def update_insta():
         node = edge["node"]
         caption_edge = node["edge_media_to_caption"]["edges"]
         caption_text = caption_edge[0]["node"]["text"] if len(caption_edge) > 0 else ""
-        posts.append(
-            {
-                "id": node["id"],
-                "url": f"https://instagram.com/p/{node['shortcode']}",
-                "thumbnail": f"instagram/{node['id']}.jpg",
-                "caption": caption_text,
-                "is_video": node["is_video"],
-            }
-        )
+        post = {
+            "id": node["id"],
+            "url": f"https://instagram.com/p/{node['shortcode']}",
+            "thumbnail": f"instagram/{node['id']}.jpg",
+            "caption": caption_text,
+            "is_video": node["is_video"],
+        }
+        posts.append(post)
+        print(f"Got post {post}")
+        print(f"Downloading image {node['id']}.jpg...")
         download_image(node["thumbnail_src"], f'{node["id"]}.jpg')
+        print(f"Downloaded image {node['id']}.jpg")
 
+    print("Writing Instagram data to src/data/instagram.json...")
     with open("src/data/instagram.json", "w") as f:
         f.write(json.dumps(posts))
+    print("Wrote Instagram data to src/data/instagram.json")
 
 
 def clear_images():
@@ -41,6 +51,7 @@ def clear_images():
 def download_image(url, filename):
     response = requests.get(url)
     if response.status_code == 200:
+        print(f"Writing image to public/instagram/{filename}")
         with open(f"public/instagram/{filename}", "wb") as f:
             f.write(response.content)
     else:
