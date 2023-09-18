1d51eb37eab63aa8fefe6b00eb48b425f7528611
Eugene Malinouski <jenyamalina@tut.by>
2023-09-18 19:10:28 +0200

    Update favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,32 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="mask-icon" color="#000" href="/safari-pinned-tab.svg"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         {montserrat.styles}
       </head>
